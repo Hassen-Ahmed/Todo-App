0b7ca4cade364c5e6ff5d224f94ef5b3866a4b98
Hassen-Ahmed <hassenbest23@gmail.com>
2023-09-14 16:29:10 +0100

    alter Axios baseURL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const todoApi = axios.create({
-  baseURL: "http://localhost:3000/",
+  // baseURL: "http://localhost:3000/",
+  baseURL: "https://todo-app-mongodb-vxfw.onrender.com/",
 });
 
 export const getAllTodo = () => {
