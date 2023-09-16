bf74da375a2c235ee4ddf54848b8561661012942
Hassen-Ahmed <hassenbest23@gmail.com>
2023-09-16 19:59:51 +0100

    add Loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Loading.css";
+
+export default function Loading() {
+  return (
+    <div className="loading">
+      <h2>Loading...</h2>
+      <ul>
+        <li className="heart-1">💚️</li>
+      </ul>
+    </div>
+  );
+}
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,52 +1,59 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 import Todo from "./Todo";
+import Loading from "./Loading";
 
 const TodoList = ({ list, setToDoList, setIsRender }) => {
   const [color, setColor] = useState("#a9a29c84");
 
   return (
     <section className="todo-list">
-      <div className="todo-list__color">
-        <div
-          className="div-1"
-          onClick={() => {
-            setColor("#d5ccc7");
-          }}
-        ></div>
-        <div
-          className="div-2"
-          onClick={() => {
-            setColor("#7ed4e184");
-          }}
-        ></div>
-        <div
-          className="div-3"
-          onClick={() => {
-            setColor("#dc9d5f84");
-          }}
-        ></div>
-        <div
-          className="div-4"
-          onClick={() => {
-            setColor("#b158e484");
-          }}
-        ></div>
-      </div>
+      {list.length ? (
+        <>
+          <div className="todo-list__color">
+            <div
+              className="div-1"
+              onClick={() => {
+                setColor("#d5ccc7");
+              }}
+            ></div>
+            <div
+              className="div-2"
+              onClick={() => {
+                setColor("#7ed4e184");
+              }}
+            ></div>
+            <div
+              className="div-3"
+              onClick={() => {
+                setColor("#dc9d5f84");
+              }}
+            ></div>
+            <div
+              className="div-4"
+              onClick={() => {
+                setColor("#b158e484");
+              }}
+            ></div>
+          </div>
 
-      {list.map((toDoItem) => {
-        return (
-          <Todo
-            key={`${toDoItem["_id"]}`}
-            todo={toDoItem.todo}
-            date={toDoItem.date}
-            setToDoList={setToDoList}
-            color={color}
-            toDoItem={toDoItem}
-            setIsRender={setIsRender}
-          />
-        );
-      })}
+          {list.map((toDoItem) => {
+            return (
+              <Todo
+                key={`${toDoItem["_id"]}`}
+                todo={toDoItem.todo}
+                date={toDoItem.date}
+                setToDoList={setToDoList}
+                color={color}
+                toDoItem={toDoItem}
+                setIsRender={setIsRender}
+              />
+            );
+          })}
+        </>
+      ) : (
+        <Loading />
+      )}
     </section>
   );
 };
