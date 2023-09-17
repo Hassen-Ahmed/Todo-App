a881c0d8b43b2edaa2936ea19255eaaa91fa65d9
Hassen-Ahmed <hassenbest23@gmail.com>
2023-09-17 21:29:30 +0100

    import and wrap main.jsx with context provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { TodoContextProvider } from "./context/ContextTodo.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <>
+  <TodoContextProvider>
     <App />
-  </>
+  </TodoContextProvider>
 );
