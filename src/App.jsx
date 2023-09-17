b038b555643ea431ccb85e0053a2ee5e45bdf478
Hassen-Ahmed <hassenbest23@gmail.com>
2023-09-17 21:28:23 +0100

    refactor the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,18 @@
-import { useEffect, useState } from "react";
 import AddTodo from "./components/AddTodo";
 import TodoList from "./components/TodoList";
-import { BsGithub } from "react-icons/bs";
-import { getAllTodo } from "./utils/api.js";
 import Timer from "./components/Timer";
 import "./App.css";
 import { Toaster } from "react-hot-toast";
-
-function getAllTodoHandler(setToDoList) {
-  getAllTodo()
-    .then(({ todos }) => {
-      const orderTodos = todos.reverse();
-      setToDoList(orderTodos);
-    })
-    .catch((err) => {
-      console.log(err);
-      setToDoList([]);
-    });
-}
+import Footer from "./components/Footer";
 
 function App() {
-  const [toDoList, setToDoList] = useState([]);
-  const [isRender, setIsRender] = useState(false);
-
-  useEffect(() => {
-    getAllTodoHandler(setToDoList);
-  }, [isRender]);
-
   return (
     <main className="app">
       <Timer />
       <Toaster />
-      <AddTodo
-        setToDoList={setToDoList}
-        getAllTodoHandler={getAllTodoHandler}
-      />
-      <TodoList
-        list={toDoList}
-        setToDoList={setToDoList}
-        setIsRender={setIsRender}
-      />
-      <div className="github-log">
-        <a
-          href="https://github.com/Hassen-Ahmed/Todo-App/tree/main"
-          target="_blank"
-        >
-          <BsGithub />
-        </a>
-        <p>
-          <span className="copyright">&copy;</span>Copyright 2023 By Hassen
-          Abdela
-        </p>
-      </div>
+      <AddTodo />
+      <TodoList />
+      <Footer />
     </main>
   );
 }
