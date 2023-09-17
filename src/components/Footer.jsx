13f57701d426abb42229a636bdd2a0f9b1658f87
Hassen-Ahmed <hassenbest23@gmail.com>
2023-09-17 21:32:58 +0100

    create Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BsGithub } from "react-icons/bs";
+
+export default function Footer() {
+  return (
+    <div className="github-log">
+      <a
+        href="https://github.com/Hassen-Ahmed/Todo-App/tree/main"
+        target="_blank"
+      >
+        <BsGithub />
+      </a>
+      <p>
+        <span className="copyright">&copy;</span>Copyright 2023 By Hassen Abdela
+      </p>
+    </div>
+  );
+}
