a605e69027f1e125e8c0b5e0ab774156d32f2a72
Hassen-Ahmed <hassenbest23@gmail.com>
2023-09-20 22:03:05 +0100

    create useContext to use theme state

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,19 @@
 import { BsGithub } from "react-icons/bs";
+import { TodoContext } from "../context/ContextTodo";
+import { useContext } from "react";
 
 export default function Footer() {
+  const { theme } = useContext(TodoContext);
   return (
     <div className="github-log">
       <a
+        style={{ color: `${theme === "light" ? "#4a4a4a" : "#989898"}` }}
         href="https://github.com/Hassen-Ahmed/Todo-App/tree/main"
         target="_blank"
       >
         <BsGithub />
       </a>
-      <p>
+      <p style={{ color: `${theme === "light" ? "#4a4a4a" : "#989898"}` }}>
         <span className="copyright">&copy;</span>Copyright 2023 By Hassen Abdela
       </p>
     </div>
