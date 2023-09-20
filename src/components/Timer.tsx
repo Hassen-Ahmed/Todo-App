db9d68304c3ad5eff15237250f47cd9397c111dc
Hassen-Ahmed <hassenbest23@gmail.com>
2023-09-20 22:03:47 +0100

    create useContext to use theme

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,7 +1,9 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { TodoContext } from "../context/ContextTodo";
 
 export default function Timer() {
   const [currentTime, setCurrentTime] = useState(new Date().toLocaleString());
+  const { theme } = useContext(TodoContext);
 
   useEffect(() => {
     setInterval(() => {
@@ -9,5 +11,9 @@ export default function Timer() {
     }, 1000);
   }, []);
 
-  return <p className="app-time">{currentTime}</p>;
+  return (
+    <p className={`${theme === "light" ? "app-time" : "app-time--light"}`}>
+      {currentTime}
+    </p>
+  );
 }
