86ba62058208745f19252acd853f37f067574c9b
Hassen-Ahmed <hassenbest23@gmail.com>
2023-09-21 23:10:42 +0100

    add Link for profile and todo page

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -5,25 +5,44 @@ import {
   BsSun,
   BsFillMoonFill,
 } from "react-icons/bs";
+import { AiOutlineHome } from "react-icons/ai";
 import { CgProfile } from "react-icons/cg";
 import "./Menu.css";
 import { TodoContext } from "../../context/ContextTodo";
+import { Link } from "react-router-dom";
 
 export default function Menu() {
   const [isDropDown, setIsDropDown] = useState(false);
   const [isSort, setIsSort] = useState(false);
   const [isLight, setIsLight] = useState(true);
   const { setIsSortedAsd, theme, setTheme } = useContext(TodoContext);
-
+  const [isProfile, setIsProfile] = useState(false);
   useEffect(() => {
     document.body.style.backgroundColor =
       theme === "dark" ? "#454545" : "#d5ccc7";
   }, [theme]);
+
   return (
     <div className="menu">
-      <div className={`${isDropDown ? " btn" : " origin-point"}`}>
-        <CgProfile />
-      </div>
+      {isProfile ? (
+        <Link
+          to="/todo"
+          style={{ color: "#333333d6" }}
+          className={`${isDropDown ? " btn" : " origin-point"}`}
+          onClick={() => setIsProfile(!isProfile)}
+        >
+          <AiOutlineHome />
+        </Link>
+      ) : (
+        <Link
+          to="/profile"
+          style={{ color: "#333333d6" }}
+          className={`${isDropDown ? " btn" : " origin-point"}`}
+          onClick={() => setIsProfile(!isProfile)}
+        >
+          <CgProfile />
+        </Link>
+      )}
 
       <div
         className={` ${isDropDown ? " btn" : " origin-point"}`}
