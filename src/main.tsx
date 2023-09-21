b3c5d2380bde2cb7d1581da44a2844712ca7da2c
Hassen-Ahmed <hassenbest23@gmail.com>
2023-09-21 23:11:19 +0100

    wrap all components with BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@
 import ReactDOM from "react-dom/client";
 import { TodoContextProvider } from "./context/ContextTodo.js";
 import App from "./App.js";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <TodoContextProvider>
-    <App />
-  </TodoContextProvider>
+  <BrowserRouter>
+    <TodoContextProvider>
+      <App />
+    </TodoContextProvider>
+  </BrowserRouter>
 );
