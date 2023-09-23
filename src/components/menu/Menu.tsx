f7dc6edc4cc84c3ac620495e68d8fb8b791e5eda
Hassen-Ahmed <hassenbest23@gmail.com>
2023-09-23 22:17:13 +0100

    modify Menu component for visiblility

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -15,60 +15,66 @@ export default function Menu() {
   const [isDropDown, setIsDropDown] = useState(false);
   const [isSort, setIsSort] = useState(false);
   const [isLight, setIsLight] = useState(true);
-  const { setIsSortedAsd, theme, setTheme } = useContext(TodoContext);
   const [isProfile, setIsProfile] = useState(false);
+  const { setIsSortedAsd, theme, setTheme, IsTherUserId } =
+    useContext(TodoContext);
+
   useEffect(() => {
     document.body.style.backgroundColor =
       theme === "dark" ? "#454545" : "#d5ccc7";
   }, [theme]);
 
   return (
-    <div className="menu">
-      {isProfile ? (
-        <Link
-          to="/todo"
-          style={{ color: "#333333d6" }}
-          className={`${isDropDown ? " btn" : " origin-point"}`}
-          onClick={() => setIsProfile(!isProfile)}
-        >
-          <AiOutlineHome />
-        </Link>
-      ) : (
-        <Link
-          to="/profile"
-          style={{ color: "#333333d6" }}
-          className={`${isDropDown ? " btn" : " origin-point"}`}
-          onClick={() => setIsProfile(!isProfile)}
-        >
-          <CgProfile />
-        </Link>
-      )}
+    <>
+      {IsTherUserId.length ? (
+        <div className="menu">
+          {isProfile ? (
+            <Link
+              to="/todo"
+              style={{ color: "#333333d6" }}
+              className={`${isDropDown ? " btn" : " origin-point"}`}
+              onClick={() => setIsProfile(!isProfile)}
+            >
+              <AiOutlineHome />
+            </Link>
+          ) : (
+            <Link
+              to="/profile"
+              style={{ color: "#333333d6" }}
+              className={`${isDropDown ? " btn" : " origin-point"}`}
+              onClick={() => setIsProfile(!isProfile)}
+            >
+              <CgProfile />
+            </Link>
+          )}
 
-      <div
-        className={` ${isDropDown ? " btn" : " origin-point"}`}
-        onClick={() => {
-          setIsSort(!isSort);
-          setIsSortedAsd((prevValue) => !prevValue);
-        }}
-      >
-        {isSort ? <BsSortNumericDown /> : <BsSortNumericUpAlt />}
-      </div>
+          <div
+            className={` ${isDropDown ? " btn" : " origin-point"}`}
+            onClick={() => {
+              setIsSort(!isSort);
+              setIsSortedAsd((prevValue) => !prevValue);
+            }}
+          >
+            {isSort ? <BsSortNumericDown /> : <BsSortNumericUpAlt />}
+          </div>
 
-      <div
-        className={` ${isDropDown ? "btn" : " origin-point"}`}
-        onClick={() => {
-          setTheme((prevTheme) => {
-            return prevTheme === "dark" ? "light" : "dark";
-          });
-          setIsLight(!isLight);
-        }}
-      >
-        {isLight ? <BsSun /> : <BsFillMoonFill />}
-      </div>
+          <div
+            className={` ${isDropDown ? "btn" : " origin-point"}`}
+            onClick={() => {
+              setTheme((prevTheme) => {
+                return prevTheme === "dark" ? "light" : "dark";
+              });
+              setIsLight(!isLight);
+            }}
+          >
+            {isLight ? <BsSun /> : <BsFillMoonFill />}
+          </div>
 
-      <div className="menu-btn" onClick={() => setIsDropDown(!isDropDown)}>
-        <p>🔘️🔘️🔘️</p>
-      </div>
-    </div>
+          <div className="menu-btn" onClick={() => setIsDropDown(!isDropDown)}>
+            <p>🔘️🔘️🔘️</p>
+          </div>
+        </div>
+      ) : null}
+    </>
   );
 }
