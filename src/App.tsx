728463a14a5ddd4fcdf0f2efe132eaf81399be12
Hassen-Ahmed <hassenbest23@gmail.com>
2023-09-29 17:08:00 +0100

    testing cookie

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import { Toaster } from "react-hot-toast";
 import Footer from "./components/Footer";
 import Menu from "./components/menu/Menu";
 import { Route, Routes } from "react-router-dom";
-import { Suspense, lazy } from "react";
+import { Suspense, lazy, useEffect } from "react";
+import { getAllTodo } from "./utils/api";
 
 const Profile = lazy(() => import("./components/profile/Profile"));
 const SignLogIn = lazy(() => import("./components/forms/SignupLogIn"));
@@ -14,6 +15,12 @@ const Signup = lazy(() => import("./components/forms/Signup"));
 const LogIn = lazy(() => import("./components/forms/LogIn"));
 
 function App() {
+  useEffect(() => {
+    getAllTodo().then((data) => {
+      console.log(data);
+      console.log(document.cookie);
+    });
+  }, []);
   return (
     <main className="app">
       <Menu />
